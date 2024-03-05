336bd46735ff2f33201c72aaccfc768af6e2bf9b
kovbalcu <126391854+kovbalcu@users.noreply.github.com>
2024-03-05 19:08:07 +0100

    Update huncutkodas.js

--- bala/huncutkodas.js
+++ bala/huncutkodas.js
@@ -1,6 +1,4 @@
-(function() {
-    window.onload = function () {
-        var cuc = document.querySelector("body > div.container.main > div:nth-child(2) > div.col-md-9.col-xs-12 > h1");
-        cuc.innerText = "cucc";
-    }
-});
+window.onload = function () {
+    var cuc = document.querySelector("body > h1");
+    cuc.innerText = "cucc";
+}
